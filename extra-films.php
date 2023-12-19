64c221c637afd8a446395203a5c7a4570a6faf58
Garage404 <bakaarion3@gmail.com>
2023-12-19 13:25:47 +0100

    fonction qui trouve tous les index

--- extra-films.php
+++ extra-films.php
@@ -8,29 +8,76 @@ $films = $brut["feed"]["entry"];
 function indent($i){
     $retour = "";
     for($j=0;$j<$i;$j++){
-        $retour .= "| &nbsp; &nbsp; ";
+        $retour .= ". &nbsp; &nbsp; &nbsp; ";
     }
     return $retour;
 }
-function vardumpPerso($tableau, $i = 0){
-    foreach($tableau as $k => $v){
-        if(is_array($v)){
-            echo ($i>0)?"<br/>":null;
-            echo indent($i)."[<b>\"$k\"</b>]";
-            vardumpPerso($v, ++$i);
-        }
-        else{
-            echo "<br/>".indent($i)."[<b>\"$k\"</b>] => $v<br/>".indent($i);
+function vardumpPerso($variable, $i = 0){
+    if(is_array($variable)){
+        foreach($variable as $k => $v){
+            if(is_array($v)){
+                echo ($i>0)?"<br/>":null;
+                echo indent($i)."[<b>\"$k\"</b>]";
+                vardumpPerso($v, ++$i);
+            }
+            else{
+                echo "<br/>".indent($i)."[<b>\"$k\"</b>] => (".gettype($v).") $v<br/>".indent($i);
+            }
         }
     }
+    else{
+        echo gettype($v)." $v";
+    }
 }
-
-vardumpPerso($films);
-//var_dump($films)
-
-
+// function trouverValeur($tableau, $valeur, $index = array()){
+//     $valeur = strtolower($valeur);
+//     $retour = array();
+//     if(is_array($tableau)){
+//         foreach($tableau as $k => $v){
+//             if(empty($retour)){
+//                 //var_dump($index);
+//                 if(is_array($v)){
+//                     $index[]=$k;
+//                     trouverValeur($v, $valeur, $index);
+//                 }
+//                 else{
+//                     if($valeur==strtolower($v)){
+//                         $index[]=$k;
+//                         $retour=$index;
+//                         //var_dump($retour);
+//                     }
+//                     else{
+//                         $index = array();
+//                     }
+//                 }
+//             }
+//         }
+//     }
+//     return $retour;
+// }
+function trouverValeur($films, $valeur, $offset = array(), &$match = false){
+    foreach($films as $k => $v){
+        if(!$match){
+            if(is_array($v)){
+                $offset[]=$k;
+                trouverValeur($v, $valeur, $offset, $match);
+            }
+            else{
+                if(strtolower($v) == strtolower($valeur)){
+                    $offset[]=$k;
+                    $match = $offset;
+                }
+            }
+        }
+    }
+    return $match;
+}
 //trouverFilm($films, "a", "a");
 ?>
 Top 10 : <?php for($i = 0; $i <10; $i++){ echo '<p>'.($i+1).') '.$films[$i]["im:name"]["label"].'</p>'; }  ?>
-<p>Classement de gravity : </p>
+<p>Classement de gravity : <?php echo ++trouverValeur($films, "gravity")[0] ?></p>
 
+
+<?php
+
+vardumpPerso($films);
