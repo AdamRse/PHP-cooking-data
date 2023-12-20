7b5650ac995fa0e16e56a3fb72d75804342ab844
Garage404 <bakaarion3@gmail.com>
2023-12-20 11:31:00 +0100

    fini v1

--- Partie-9/1.php
+++ Partie-9/1.php
@@ -1,4 +1,5 @@
 <?php
+setLocale(LC_TIME, 'fr_FR.UTF-8');
 function traduire($v){
     $v=strtolower($v);
     $r = "";
@@ -10,8 +11,6 @@ function traduire($v){
         case "friday"; $r="vendredi"; break;
         case "saturday"; $r="samedi"; break;
         case "sunday"; $r="dimanche"; break;
-
-
         case "january"; $r="janvier"; break;
         case "february"; $r="fevrier"; break;
         case "march"; $r="mars"; break;
@@ -35,6 +34,21 @@ echo "<p>".date("d-m-y", time())."</p>";
 //3
 echo "<p>".date("l F Y", time())."</p>";
 //4
-echo "<p>".date("U", time())."</p>";
+echo "<p>Timestamp actuel :".date("U", time());
+$date1 = new DateTime("2016-02-08 15:00:00");
+echo "<br/>Timestamp du 2 août 2016 à 15h : ".date("U", 1470142800)."</p>";
 //5
-echo "<p>".date("U", time())."</p>";
+$date1 = new DateTime();
+$date2 = new DateTime();
+$date2->setTimestamp(1470142800);
+echo "<p>Il y a ".$date2->diff($date1)->days." jours entre aujourd'hui et le 2 août 2016.</p>";
+//6
+$date1 = new DateTime("2016-02-01");
+$date2 = new DateTime("2016-03-01");
+echo "<p>Il y a ".$date2->diff($date1)->days." jours en février 2016</p>";
+//7
+$date1->setTimestamp(time()+(3600*24*20));
+echo "<p>Dans 20 jours on sera le ".$date1->format("d/m/Y")."</p>";
+//8
+$date1->setTimestamp(time()-(3600*24*22));
+echo "<p> ".$date1->format("d/m/Y")."</p>";
